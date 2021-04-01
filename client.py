bc2929d0ace35ab789362a52da420f399c1d6c54
erikjandz <43314178+erikjandz@users.noreply.github.com>
2021-04-01 09:18:19 +0200

    first version

---
+++ client.py
@@ -0,0 +1,48 @@
+import socket
+
+# Some global variables
+HEADER = 64
+PORT = 5050
+SERVER = "192.168.99.1"
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+print("[CONNECTED] connected to server...")
+
+
+def writeFile(content):
+    f = open("client.txt", "w")
+    f.write(content)
+    f.close()
+
+
+def handle_server():
+    try:
+        while True:
+            message = client.recv(2048).decode(FORMAT)
+            writeFile(message)
+            print("[FILE UPDATED] client.txt got updated.")
+
+    except:
+        print("[DISCONNECTED] we disconnected from the server.")
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print("[MESSAGE SENT] message sent to server.")
+
+
+send("Connected")
+writeFile(client.recv(2048).decode(FORMAT))
+
+handle_server()
+
+# send(DISCONNECT_MESSAGE)
---
+++ server.py
@@ -0,0 +1,111 @@
+import socket
+import threading
+import time
+
+# Some global variables
+HEADER = 64
+PORT = 5050
+SERVER = "0.0.0.0" #socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+# Simple function to read from the server.txt file and return the content as a string
+def readFile():
+    f = open("server.txt", "r")
+    text = f.read()
+    f.close()
+    return text
+
+
+# Send the file updated somewhere else to all the clients
+def updated_file_to_clients(registered_connections, new_file):
+    for conn in registered_connections:
+        conn.send(new_file.encode(FORMAT))
+    print("[UPDATE ALL CLIENTS FILES] all the files from the clients are updated")
+
+
+# If the server.txt changes, this thread will send the changed file to the clients
+def handle_file_changes(registered_connections: list, dummy_item):
+    last_file_content = readFile()
+    while True:
+        new_file_content = readFile()
+        if last_file_content != new_file_content:
+            last_file_content = new_file_content
+            print("[FILE CHANGED] the server.txt file changed content")
+            updated_file_to_clients(registered_connections, last_file_content)
+        time.sleep(1)
+
+
+# simple function to reeive a message from a client
+def recv(conn):
+    msg_length = conn.recv(HEADER).decode(FORMAT)
+    if msg_length:
+        msg_length = int(msg_length)
+        return conn.recv(msg_length).decode(FORMAT)
+    return False
+
+
+# Thread for every client so the server can listen to all of them if they want to disconnect
+def handle_client(registered_connections, conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    try:
+        # get the first message from the client
+        msg = recv(conn)
+        if msg:
+            print(f"[{addr}] {msg}")
+
+        # then send the current content of the server.txt file
+        conn.send(readFile().encode(FORMAT))
+
+        # then keep listening to the client till they disconnect or send a message
+        while connected:
+            msg = recv(conn)
+            if msg:
+                if msg == DISCONNECT_MESSAGE:
+                    print(f"[DISCONNECTED] {addr} disconnected.")
+                    connected = False
+                    registered_connections.remove(conn)
+                    conn.close()
+
+                print(f"[{addr}] {msg}")
+
+    # When an error occurred it's most likely because the client disconnected so we remove him
+    # and end the thread by leaving the function
+    except:
+        print(f"[DISCONNECTED] {addr} disconnected.")
+        registered_connections.remove(conn)
+        conn.close()
+
+
+# Loop to register new clients
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+
+    registered_connections = set()
+
+    # start a thread that checks the server.txt file for updates
+    # has a dummy item because f*king python keeps interpreting the registered_connection
+    # as a list of args instead of one arg
+    file_handle_thread = threading.Thread(target=handle_file_changes, args=(registered_connections, "dummy_item"))
+    file_handle_thread.start()
+
+    # infinite loop to look for new clients to accept and start a thread for every new client that listens to it
+    while True:
+        conn, addr = server.accept()
+        registered_connections.add(conn)
+
+        # start a thread for the specific client to listen to him
+        client_handle_thread = threading.Thread(target=handle_client, args=(registered_connections, conn, addr))
+        client_handle_thread.start()
+        print(f"[ACTIVE CONNECTIONS] {len(registered_connections)}")
+
+
+print("[STARTING] server is starting...")
+start()
